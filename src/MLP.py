d33c7311f1b17df121158410a903fb6bd4b9169f
imantha603 <imanthasadamayura1322@gmail.com>
2026-03-12 02:04:57 +0000

    s change

--- src/MLP.py
+++ src/MLP.py
@@ -42,11 +42,11 @@ class MultiLayerPerceptron:
         
         for i in range(len(self.layers)):
             self.values.append([])
-            self.networks.append([])
+            self.network.append([])
             self.values[i]=[0.0 for j in range(len(self.layers[i]))]
             if i>0: #network[0] is the input layer, so it has no neurons
                 for j in range(self.layers[i]):
-                    self.network.append(Perceptron(inputs=self.layer[i-1],bias=self.bias))
+                    self.network.append(Perceptron(inputs=self.layers[i-1],bias=self.bias))
         
         self.network = np.array([np.array(x) for x in self.network],dtype=object)
         self.values = np.array([np.array(x) for x in self.values],dtype=object)
